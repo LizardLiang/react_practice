72f10765bca406b275602709225e857b5cfa539d
lizard <shotup0101@gmail.com>
2020-11-13 23:20:09 +0800

    add form checkbox and dynamic label

--- app.jsx
+++ app.jsx
@@ -1,57 +1,78 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// let element = <h1>Hello Lizard</h1>;
-
-// const gettime = () => {
-//     let datetime = (
-//         <div>
-//             <span>current time:{new Date().toLocaleTimeString()}</span>
-//         </div>
-//     )
-//     ReactDom.render(
-//         datetime,
-//         document.getElementById('time_area')
-//     )
-// }
-
-class Tree extends React.Component {
-    render(){
-        return <div>Hello, King<br/>Hello, Queen<br/></div>
-    }
-}
-
-// ReactDom.render(tree, document.getElementById('root'))
-
-class GetTime extends React.Component {
-    // 建構 state
+class CreateForm extends React.Component {
     constructor(props) {
-        // 取得上層的props
         super(props)
-
         this.state = {
-            time: new Date().toLocaleTimeString()
+            name: 'Default Name',
+            sub_name: '',
+            placehold: 'Input Name',
+            lists: [
+                    {TaskID: '01', TaskName: 'PUBG', check: false},
+                    {TaskID: '02', TaskName: 'APEX', check: false},
+                    {TaskID: '03', TaskName: 'MHW', check: false},
+                    ],
+            checkItem: [],
+            show: false
         }
+        this.changeState = this.changeState.bind(this)
+        this.submitForm = this.submitForm.bind(this)
     }
 
-    componentDidMount() {
-        const countup = () => {
-            this.setState({time: new Date().toLocaleTimeString()})
-        }
-
-        setInterval(countup, 1000)
+    changeState(event){
+        this.setState({name:event.target.value})
     }
 
-    render() {
-        return <span>Current Time: {this.state.time}</span>
+    submitForm(event){
+        let tmp = ''
+        this.setState({sub_name:this.state.name})
+        this.state.lists.map(
+            (list)=>{
+                (list.check) ? tmp += list.TaskName : '\n'
+            })
+        this.state.show = this.state.show ^ 1
+        this.setState({checkItem: tmp})
+        // prevent react submit all html content
+        event.preventDefault()
+    }
+
+    checkStateChange(index){
+        let arr_  = this.state.lists;
+        arr_[index].check = arr_[index].check ^ 1;
+        this.setState({lists: arr_})
+    }
+
+    render () {
+        let lists = this.state.lists.map((list, index)=>(
+            <div key={list.TaskID}>
+                <input  type="checkbox"
+                        checked={list.check}
+                        onChange={this.checkStateChange.bind(this, index)}
+                        key={list.TaskID}/>
+                <label>{list.TaskName}</label>
+            </div>
+        ));
+        return (
+            <form onSubmit={this.submitForm}>
+                <label>Name: </label>
+                <input  id="name" 
+                        name="name"
+                        value={this.state.name}
+                        placehold={this.state.placehold}
+                        onChange={this.changeState}/>
+                <br/>
+                {lists}
+                <input id="submit" type="submit" value="submit"></input>
+                <br/>
+                {(this.state.lists.map((item, index) => 
+                        (item.check) ? 
+                        <div style={{visibility: item.check ? 'visible' : 'hidden'}}>
+                            <span>{item.TaskName}</span><br/>
+                        </div> : ''))}
+            </form>
+        )
     }
 }
 
-ReactDom.render(<div><Tree /><GetTime /></div>, document.getElementById('time_area'))
-
-// ReactDom.render(
-//     element,
-//     document.getElementById('root')
-// );
-
-// setInterval(gettime,1000)
+ReactDom.render(<CreateForm />, document.getElementById('root'))
