36bb6bf8e8147bda2120128a45a6e826e858c239
Lizard <shotup0101@gmail.com>
2020-11-30 16:01:33 +0800

    add experience and education info

--- components/pagebox/experience.jsx
+++ components/pagebox/experience.jsx
@@ -9,17 +9,51 @@ class Experience extends React.Component    {
     render(){
         const Styles = {
             StyleContainer:{
-                color:      'white',
-                fontSize:   '4vmin',
-                background:  'rgb(64, 68, 75)'
+                color:          'white',
+                fontSize:       '3vmin',
+                background:     'rgb(64, 68, 75)',
+                height:         'auto',
+                overflow:       'auto'
+            },
+            overallTitle:{
+                display:        'block',
+                textAlign:      'center',
+                borderBottom:   '1px solid grey'
+            },
+            leftElement:{
+                float:  'left',
+                width:  '50%'
+
+            },
+            rightElement:{
+                float:  'right',
+                width:  '50%'
             }
         }
         return(
-            <div style={Styles.StyleContainer}>
-                <span>My Experience</span>
-                <ul>
+            <div style={Styles.StyleContainer} id={this.props.id}>
+                <span style={Styles.overallTitle}>簡歷</span>
+                {/* <ul>
                     <li className="li">偉祺電機 (Famax) 3 years as Software engineer</li>
-                </ul>
+                </ul> */}
+                <div style={Styles.leftElement}>
+                    工作經歷:
+                    <ul>
+                        <li className="li">
+                            偉祺電機 2017/11 ~ 現在<br/>
+                            擔任軟體工程師
+                        </li>
+                    </ul>
+                </div>
+                <div style={Styles.rightElement}>
+                    學歷:
+                    <ul>
+                        <li className="li">
+                            元智大學<br/>
+                            電機工程學系
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
