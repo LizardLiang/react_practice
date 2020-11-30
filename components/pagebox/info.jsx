08ba61484a29aa7cad2c341069a90b43c6e9afd9
Lizard <shotup0101@gmail.com>
2020-11-30 16:03:06 +0800

    add picture and contact

--- components/pagebox/info.jsx
+++ components/pagebox/info.jsx
@@ -5,8 +5,9 @@ class SchoolInfo extends Component{
     render(){
         const Styles = {
             BlockContainer:{
-                position:   'relative',
-                background:  'rgb(64, 68, 75)'
+                position:       'relative',
+                background:     'rgb(64, 68, 75)',
+                marginTop:      '10%'
             },
             liContainer:{
                 paddingLeft:    '10%'
@@ -30,15 +31,29 @@ class Info extends React.Component  {
             StyleContainer:{
                 color:      'white',
                 minHeight:  '25%',
-                fontSize:   '4vmin',
+                fontSize:   '3vmin',
                 background:  'rgb(64, 68, 75)'
+            },
+            ImgContainer:{
+                position:   'absolute',
+                top:        '0',
+                left:       '0'
+            },
+            spanContainer:{
+                marginLeft: '20%'
             }
         }
         return(
-            <div style={Styles.StyleContainer}>
-                <span>Hi! Welcome to My website</span><br/>
-                <span>My name is Fang-Ming, Liang</span><br/><br/>
-                <SchoolInfo />
+            <div style={Styles.StyleContainer} id={this.props.id}>
+                <img style={Styles.ImgContainer} src="../img/SelfPic.jpg"></img>
+                <span style={Styles.spanContainer}>梁芳銘</span><br/>
+                <span style={Styles.spanContainer}>Fang-Ming, Liang</span><br/>
+                <span style={Styles.spanContainer} role="img" aria-label="phone">
+                    📱 0920173978
+                </span><br/>
+                <span style={Styles.spanContainer} role="img" aria-label="mail">
+                    ✉️ shotup0101@gmail.com
+                </span><br/>
             </div>
         )
     }
