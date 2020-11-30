94c48b2b59f41002286f80e7466a08d408ae087f
Lizard <shotup0101@gmail.com>
2020-11-30 15:58:27 +0800

    add additioninfo button remove unused button

--- Main/SideBar.jsx
+++ Main/SideBar.jsx
@@ -6,15 +6,16 @@ class SideBar extends React.Component{
     render(){
         const Styles = {
             StyleContainer: {
-                position:   'sticky',
+                position:   'fixed',
                 top:        '25vh',
-                height:     '450px',
+                height:     '300px',
                 background: 'rgb(32, 34, 37)',
                 width:      '5vw',
                 float:      'left',
                 boxSizing:  'border-box',
                 maxWidth:   '130px',
-                minWidth:   '83.94px'
+                minWidth:   '83.94px',
+                zIndex:     '2'
             },
             offsetContainer:    {
                 position:   'relative',
@@ -26,11 +27,11 @@ class SideBar extends React.Component{
         return (
             <div style={StyleContainer}>
                 <div style={Styles.offsetContainer}>
-                    <Buttons name=""  item="5"/>
                     <Buttons name="Info"  item="1"/>
                     <Buttons name="Experience" item="2" />
                     <Buttons name="Project" item="3" />
                     <Buttons name="Contact" item="4" />
+                    <Buttons name="Github" item="5" />
                 </div>
             </div>
         )
