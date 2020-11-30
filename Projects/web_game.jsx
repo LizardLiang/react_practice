bdab04c6886c0a63f0370171b4e7ebe122f61737
lizard <shotup0101@gmail.com>
2020-11-30 21:54:54 +0800

    Add git url

--- Projects/web_game.jsx
+++ Projects/web_game.jsx
@@ -24,11 +24,15 @@ class WebGame extends Component{
         return (
             <div style={Styles.divContainer}>
                 <span style={Styles.spanContainer}>
-                    此遊戲是基於 ReactJS 所製作的小遊戲<br/>
+                    此遊戲是基於 ReactJS 所製作的小遊戲，且為RWD網頁<br/>
                     遊戲機制為在時限之內將點擊出所有汙染源<br/>
                     共有八種汙染源，隨機產生五種汙染源作為目標，並限時一分鐘內找出這五種汙染源<br/>
                     人物身上的能量條亦會隨著汙染源被找出改變其狀態<br/>
-                    當遊戲結束時根據是否找出所有汙染源，還是計時器結束，顯示訊息框。
+                    當遊戲結束時根據是否找出所有汙染源，還是計時器結束，顯示訊息框。<br/>
+                    <a href="https://github.com/lizrad/AirPollutionWithReactJS"
+                        target="_blank">
+                        原碼連結
+                    </a>
                 </span>
                 <iframe style={Styles.iframeContainer} src="http://114.32.157.74" 
                         scrolling='no'/>
