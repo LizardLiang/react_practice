c01d471b007f058a9f89ccf76b22f9603b7e8acc
Lizard <shotup0101@gmail.com>
2020-12-03 15:45:43 +0800

    add class property plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     module: {
         rules: [
             //第一個loader編譯JSX
-            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react', '@babel/preset-env'] } } },
+            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react', '@babel/preset-env'], plugins:['@babel/plugin-proposal-class-properties'] } } },
             //第二個loader編譯ES6
             { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
             //第二個loader編譯ES6
