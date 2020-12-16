522363c66e2b59d4e412ebb289c45c8beaef3500
Lizard <shotup0101@gmail.com>
2020-12-16 17:33:30 +0800

    Move header here

--- components/pagebox/home.jsx
+++ components/pagebox/home.jsx
@@ -1,6 +1,26 @@
 import React, {Component} from 'react'
+import styles from 'styled-components'
 import { Info, Experience, Projects, MyTool, AdditionInfo, Intro } from "./"
 import { SideBar } from '../sidebar'
+
+const HeaderDiv = styles.div `
+    position:       fixed;
+    top:            0px;
+    left:           0px;
+    height:         100vh;
+    width:          100vw;
+    display:        table;
+    background:     rgb(32, 34, 37);
+    -webkit-backface-visibility: hidden;
+`
+
+const HeaderH1 = styles.h1 `
+    display:        table-cell;
+    text-align:     center;
+    vertical-align: middle;
+    font-size:      5rem;
+    color:          white
+`
 
 class Home extends Component{
     render(){
@@ -10,17 +30,24 @@ class Home extends Component{
                 marginLeft: 'auto',
                 marginRight:'auto',
                 position:   'relative',
-                background: 'rgb(54, 57, 63)',
+                // background: 'rgb(54, 57, 63)',
+                background: 'rgb(32, 34, 37)',
                 boxSizing:  'border-box',
-                width:      '50vw',
+                width:      '100vw',
             },
             Container:  {
-                background: 'rgb(47, 49, 54)',
+                // background: 'rgb(47, 49, 54)',
+                background: 'rgb(32, 34, 37)',
                 position:   'relative'
             }
         }
         return(
             <div class='container_fluid' style={Styles.Container}>
+                <HeaderDiv>
+                    <HeaderH1>
+                        Hi! Everyone
+                    </HeaderH1>
+                </HeaderDiv>
                 <div style={Styles.MainPageContainer}>
                     <Info           id="1" />
                     <Intro                 />
@@ -29,7 +56,7 @@ class Home extends Component{
                     <MyTool         id="4" />
                     <AdditionInfo   id="5" />
                 </div>
-                <SideBar />
+                {/* <SideBar /> */}
             </div>
         )
     }
