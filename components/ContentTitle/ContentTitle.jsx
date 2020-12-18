53ebcdf8ffde3e4781978e0cb369b64257b7392e
Lizard <shotup0101@gmail.com>
2020-12-18 14:38:01 +0800

    Use styled component to replace original Css-in-Js

--- components/ContentTitle/ContentTitle.jsx
+++ components/ContentTitle/ContentTitle.jsx
@@ -1,5 +1,35 @@
 import React from 'React'
+import styles from 'styled-components'
 
+const MainDiv = styles.div `
+    boxSizing:          border-box;
+    height:             6%;
+    display:            block;
+    textAlign:          left;
+`
+
+// Button for title
+const ProjectButton = styles.button `
+    position:           relative;
+    width:              2%;
+    height:             100%;
+    float:              left;
+    border:             none;
+    box-shadow:         none;
+    background:         rgb(32, 34, 37);
+    font-size:          2vmin;
+    color:              white;
+    line-height:        50px;              // To set span text vertical center
+`
+
+// Span contain title
+const TitleSpan = styles.span `
+    height:             100%;
+    fontSize:           3vmin;
+    display:            block;
+    lineHeight:         50px;
+    verticalAlign:      middle;
+`
 
 class ContentTitle extends React.Component{
     render(){
@@ -11,19 +41,6 @@ class ContentTitle extends React.Component{
                 display:        'block',
                 textAlign:      'left'
             },
-            // Button before title
-            buttonContainer:{
-                position:       'relative',
-                width:          '2%',
-                height:         '100%',
-                float:          'left',
-                border:         'none',
-                boxShadow:      'none',
-                background:     'rgb(68, 68, 76)',
-                fontSize:       '2vmin',
-                color:          'white',
-                lineHeight:     '50px',
-            },
             // Title text block
             spanTitleContainer: {
                 height:         '100%',
@@ -34,14 +51,15 @@ class ContentTitle extends React.Component{
             }
         }
         return (
-            <div style={Styles.divTitleContainer}>
-                <button onClick={() => this.props.clickevent()} style={Styles.buttonContainer}>
+            <MainDiv>
+                <ProjectButton 
+                    onClick={() => this.props.clickevent()}>
                     {this.props.isVisible === 'none' ? '+' : '-'}
-                </button>
-                <span style={Styles.spanTitleContainer}>
+                </ProjectButton>
+                <TitleSpan style={Styles.spanTitleContainer}>
                     {this.props.titletext}
-                </span>
-            </div>
+                </TitleSpan>
+            </MainDiv>
         )
     }
 }
