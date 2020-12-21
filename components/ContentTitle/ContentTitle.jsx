87500d43f6b4e6d7e701168a00b61f4f4eb39adc
lizard <shotup0101@gmail.com>
2020-12-21 09:47:44 +0800

    fix font size

--- components/ContentTitle/ContentTitle.jsx
+++ components/ContentTitle/ContentTitle.jsx
@@ -1,60 +1,67 @@
-import React from 'React'
+import React from 'react'
 import styles from 'styled-components'
 
 // An overall div contain all content
-const MainDiv = styles.div `
+const MainDivCT = styles.div `
     box-sizing:         border-box;
     width:              95%;
-    height:             6%;
+    height:             40px;
     display:            block;
     text-align:         left;
     margin:             0 auto;
+    @media screen and (max-width:   1079px){
+        height          30px;
+    }
 `
 
 // Button for title
-const ProjectButton = styles.button `
-    position:           relative;
-    width:              2%;
-    height:             100%;
-    float:              left;
-    border:             none;
-    box-shadow:         none;
-    background:         rgb(32, 34, 37);
-    font-size:          2vmin;
-    color:              white;
-    line-height:        50px;              // To set span text vertical center
+const ProjectButtonCT = styles.button `
+    position:               relative;
+    display:                inline-flex;
+    width:                  auto;
+    height:                 100%;
+    float:                  left;
+    border:                 none;
+    box-shadow:             none;
+    background:             rgb(32, 34, 37);
+    font-size:              3vmin;
+    color:                  white;
+    //line-height:            50px;              // To set span text vertical center
+    align-items:            center;
+    padding:                0;
+    vertical-align:         top;
 
     &:hover {
-        cursor:         pointer;
+        cursor:             pointer;
     }
 
     &:focus {
-        outline:        none;
+        outline:            none;
     }
 `
 
 // Span contain title
-const TitleSpan = styles.span `
-    display:            inline-block;
-    height:             100%;
-    font-size:          3vmin;
-    line-height:        50px;
-    vertical-align:     middle;
-    margin-left:        5px;
+const TitleSpanCT = styles.span `
+    display:                inline-flex;
+    height:                 100%;
+    font-size:              3vmin;
+    // line-height:            50px;
+    align-items:            center;
+    margin-left:            5px;
 `
 
 class ContentTitle extends React.Component{
     render(){
         return (
-            <MainDiv>
-                <ProjectButton 
+            <MainDivCT>
+                <ProjectButtonCT 
                     onClick={() => this.props.clickevent()}>
                     {this.props.isVisible === 'none' ? '+' : '-'}
-                </ProjectButton>
-                <TitleSpan>
+                </ProjectButtonCT>
+                <TitleSpanCT>
                     {this.props.titletext}
-                </TitleSpan>
-            </MainDiv>
+                </TitleSpanCT>
+            </MainDivCT>
         )
     }
 }
