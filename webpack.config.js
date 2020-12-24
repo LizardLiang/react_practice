d3ce871ee7e8988566c0eb898dcf4bb6a0122c1c
Lizard <shotup0101@gmail.com>
2020-12-24 14:12:35 +0800

    add proxy and before setup to use POST in dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const bodyParser = require('body-parser')
+var request = require('sync-request');
+
 module.exports = {
     entry: ['./app.jsx'],
     output: {
@@ -14,6 +17,24 @@ module.exports = {
         ]
     },
     devServer:{
+        before: function(app) {
+            app.use(bodyParser.json());
+            app.use(bodyParser.urlencoded({
+                extended: true
+            }));
+
+            // Assign every POST request to this web url
+            app.post('/api/v1/', function(req, res) {
+                var serviceCallResponse = request('POST', 'http://114.32.157.74/PythonFlask/api/v1/' + req.originalUrl, {
+                    json:req.body
+                });
+                res.send(serviceCallResponse.getBody('utf8'));
+            });
+        },
+        proxy: {
+            // 指定代理服務器，所有 /api 的請求會被發送至後面的網址
+            '/api/v1/': 'http://114.32.157.74/PythonFlask/api/v1/'
+        },
         port:8082
     }
 };
