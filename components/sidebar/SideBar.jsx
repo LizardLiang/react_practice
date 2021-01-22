41f23a6645f876956aebad40c75b11ee580cf35e
Lizard <shotup0101@gmail.com>
2021-01-22 15:42:12 +0800

    removed all projects's link and add a main project page to navigate

--- components/sidebar/SideBar.jsx
+++ components/sidebar/SideBar.jsx
@@ -1,70 +1,52 @@
 import React, { Component }    from "react"
-import ReactDOM from "react-dom"
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
-import {Buttons}    from "../buttons"
-import MenuIcon from '../../img/drawsvg.svg'
+// import MenuIcon from '../../img/drawsvg.svg'
 
-class SideBar extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isVisible: 'hidden'
-        }
-        // this.handleScrollEvent = this.handleScrollEvent.bind(this)
-    }
+const PLMainDiv = styled.div ` 
+    position: absolute;
+    width: 10%;
+    display: none;
+    flex-direction: column;
+    background: inherit;
+    border-radius: 5px;
+    top: 3rem;
 
-    componentDidMount(){
-        window.addEventListener('scroll', this.handleScrollEvent)
+    &:hover {
+        display: flex;
     }
+`
+const ProjectList = () => {
 
-    componentWillUnmount(){
-        window.removeEventListener('scroll', this.handleScrollEvent)
-    }
-
-    handleScrollEvent = () => {
-        if(window.pageYOffset >= 2393){
-            this.setState({isVisible: 'visible'})
-        }
-        else{
-            this.setState({isVisible: 'hidden'})
-        }
-    }
-
-    render(){
-        const Styles = {
-            StyleContainer: {
-                position:   'fixed',
-                top:        '25vh',
-                height:     '300px',
-                background: 'rgb(32, 34, 37)',
-                width:      '5vw',
-                float:      'left',
-                boxSizing:  'border-box',
-                maxWidth:   '130px',
-                minWidth:   '83.94px',
-                zIndex:     '2',
-                visibility: this.state.isVisible
-            },
-            offsetContainer:    {
-                position:   'relative',
-                top:        '0%',
-                height:     '300px'
-            }
-        }
-        const {StyleContainer} = Styles
-        return (
-            <div ref={(MainElement) => {this.MainElement = MainElement}} style={StyleContainer}>
-                <div style={Styles.offsetContainer}>
-                    <Buttons name="Info"  item="1"/>
-                    <Buttons name="Experience" item="2" />
-                    <Buttons name="Project" item="3" />
-                    <Buttons name="Tools" item="4" />
-                    <Buttons name="Github" item="5" />
-                </div>
-            </div>
-        )
-    }
+    return (
+        <PLMainDiv>
+            <GuideButton>
+                <GuideLink to='/StockSearch'>
+                    股票查詢
+                </GuideLink>
+            </GuideButton>
+            <GuideButton>
+                <GuideLink to='./CountDown'>
+                    計時器
+                </GuideLink>
+            </GuideButton>
+            <GuideButton>
+                <GuideLink to='./CheckIP'>
+                    IP 查詢
+                </GuideLink>
+            </GuideButton>
+            <GuideButton>
+                <GuideLink to='./Scripts'>
+                    檔案下載
+                </GuideLink>
+            </GuideButton>
+            <GuideButton>
+                <GuideLink to='./TodoList'>
+                    待辦事項
+                </GuideLink>
+            </GuideButton>
+        </PLMainDiv>
+    )
 }
 
 const MainDiv = styled.div `
@@ -77,22 +59,20 @@ const MainDiv = styled.div `
     z-index:            3;
     color:              white;
     justify-content:    flex-end;
-    background:         rgb(32, 34, 37);
-    box-shadow:         0 0 10px 20px rgba(0, 0, 0, 0.5);
-
-    
+    background:     rgb(47, 49, 54);
+    /* box-shadow:         0 0 10px 20px rgba(0, 0, 0, 0.5); */
 `
 
 const GuideButton = styled.button `
     min-width:          4rem;
     width:              auto;
     align-content:      center;
-    background:         rgb(32, 34, 37);
+    background:         inherit;
     border:             none;
     color:              white;
     font-size:          2rem;
     opacity:            0.3;
-    margin:             0 2rem;
+    margin:             0 0.5rem;
 
     @media screen and (max-width:   768px){
         font-size:      1.41rem;
@@ -123,6 +103,14 @@ const GuideButton = styled.button `
         opacity:        1;
     }
 
+    &:focus {
+        opacity: 1;
+        outline: none;
+        div {
+            display: flex;
+        }
+    }
+
     a :hover{
         text-decoration: none;
     }
@@ -144,28 +132,13 @@ export default class HeadBanner extends Component {
                     </GuideLink> 
                 </GuideButton>
                 <GuideButton>
-                    <GuideLink to='/StockSearch'>
-                        股票查詢
+                    <GuideLink to='/Projects'>
+                        Projects
                     </GuideLink>
                 </GuideButton>
                 <GuideButton>
-                    <GuideLink to='./CountDown'>
-                        計時器
-                    </GuideLink>
-                </GuideButton>
-                <GuideButton>
-                    <GuideLink to='./CheckIP'>
-                        IP 查詢
-                    </GuideLink>
-                </GuideButton>
-                <GuideButton>
-                    <GuideLink to='./Scripts'>
-                        檔案下載
-                    </GuideLink>
-                </GuideButton>
-                <GuideButton>
-                    <GuideLink to='./TodoList'>
-                        待辦事項
+                    <GuideLink to='/Login'>
+                        登入/註冊
                     </GuideLink>
                 </GuideButton>
                 {/* <img src={MenuIcon} alt='設定' /> */}
