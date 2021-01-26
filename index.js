b611191a72b4513dbb9ee7601b677831f0ef74f1
Lizard <shotup0101@gmail.com>
2021-01-26 09:20:30 +0800

    create reducer to modify login status

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export * from './app.jsx'
+import {createContext} from 'react'
 import {createStore} from "redux" // import createStore component from redux
 
 {/*
@@ -6,33 +7,21 @@ import {createStore} from "redux" // import createStore component from redux
     structure message:
         list of msg with structure key, name, messge
 */}
-const data =    {
-    message:
-    [ 
-        {key:"1",name:'神Q',message:'嗨！大家好啊！'},
-        {key:"2",name:'小馬',message:'早安啊！昨天有沒有好好發文？'},
-        {key:"3",name:'王子',message:'ㄛ！別說了，那真的超級累！'},
-        {key:"4",name:'神Q',message:'哈哈哈！加油啦！再一下就結束了！'},
-        {key:"5",name:'王子',message:'結束後我一定要爆睡一頓！'},
-    ]
-}
 
-// Set action to call by react program
-const addMessage = article => ({type:'addMessage', payload:article})
+// Account status 
+// status: 0 - not logged in
+//         1 - logged in
+// account: account Name
+const accountStatus = {status: 0, account: ''}
 
-const rootReducer = (state = data, action) => {
+function accountReducer(state, action){
     switch(action.type){
-        case "addMessage":
-            action.payload.key = String(state.message.length  + 1)
-            return {...state, message: [...state.message, action.payload]}
+        case 'set_account':
+            // need to return a new Obj to make context update
+            return Object.assign({}, state, {status: action.payload.status, account: action.payload.account});
         default:
-            return state
+            return accountStatus;
     }
 }
 
-// Setup a store to store reducer
-const store = createStore(rootReducer)
-
-window.store = store
-
-export  default store
+export {accountReducer, accountStatus}
